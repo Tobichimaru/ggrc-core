4a733a60d34afb3254de06f71e289e009b5329f3
Kate Zabelova <kzabelova@gmail.com>
2018-07-17 13:46:55 +0300

    Move comment deletion into separate handler

--- src/ggrc/models/hooks/relationship.py
+++ src/ggrc/models/hooks/relationship.py
@@ -142,6 +142,13 @@ def handle_del_audit_issue_mapping(instances):
     src, dst = _order(instance.source, instance.destination)
     if _is_audit_issue(src, dst):
       _handle_del_audit_issue_mapping(audit=src, issue=dst)
+
+
+@from_session(all_models.Relationship, deleted=True)
+def handle_del_comment_mapping(instances):
+  """Handle delete of commentable objects. """
+  for instance in instances:
+    _, dst = _order(instance.source, instance.destination)
     if dst.type == all_models.Comment.__name__:
       db.session.query(all_models.Comment).filter(
           all_models.Comment.id == dst.id
@@ -327,6 +334,8 @@ def init_hook():  # noqa
                   handle_new_audit_issue_mapping)
   sa.event.listen(sa.orm.session.Session, "before_flush",
                   handle_del_audit_issue_mapping)
+  sa.event.listen(sa.orm.session.Session, "before_flush",
+                  handle_del_comment_mapping)
 
   # Event listener for relationship delete operation validate.
   sa.event.listen(
